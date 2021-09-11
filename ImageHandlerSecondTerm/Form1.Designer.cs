0f85c1f437c12096d2c81a6efaaad0f351539c69
avaneszdes <avaneszdes@gmail.com>
2021-09-11 09:35:57 +0300

    Add project files.

---
+++ ImageHandlerSecondTerm/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace ImageHandlerSecondTerm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(39, 34);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(253)))), ((int)(((byte)(239)))));
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 175);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(475, 274);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 67);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(78, 86);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseHover += new System.EventHandler(this.pictureBox1_MouseHover);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Angle";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(65, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(54, 23);
+            this.textBox1.TabIndex = 4;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(253)))), ((int)(((byte)(239)))));
+            this.dataGridView2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(528, 175);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 25;
+            this.dataGridView2.Size = new System.Drawing.Size(475, 274);
+            this.dataGridView2.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(228)))), ((int)(((byte)(166)))));
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1214, 61);
+            this.panel1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(253)))), ((int)(((byte)(239)))));
+            this.ClientSize = new System.Drawing.Size(1214, 594);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ ImageHandlerSecondTerm/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageHandlerSecondTerm
+{
+    public partial class Form1 : Form
+    {
+        Bitmap firstBitmap;
+        List<int> listOfDifferentNumbers;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\",
+                Title = "Browse Text Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "bmp",
+                Filter = "bmp files (*.bmp)|*.bmp",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                dataGridView1.Rows.Clear();
+                var file = openFileDialog1.FileName;
+                listOfDifferentNumbers = new List<int>();
+
+
+                firstBitmap = new Bitmap(file);
+                pictureBox1.Image = firstBitmap;
+
+                var polBitmap = new Bitmap(firstBitmap);
+
+                for (int i = 0; i < firstBitmap.Width; i++)
+                {
+                    for (int j = 0; j < firstBitmap.Height; j++)
+                    {
+                        var color = (polBitmap.GetPixel(i, j).R + polBitmap.GetPixel(i, j).G + polBitmap.GetPixel(i, j).B) / 3;
+                        polBitmap.SetPixel(i, j, Color.FromArgb(color, color, color));
+                    }
+                }
+
+
+                pictureBox1.Image = polBitmap;
+                for (int i = 0; i < firstBitmap.Width; i++)
+                {
+                    dataGridView1.Columns.Add(i + 1 + "", i + 1 + "");
+
+                }
+
+                dataGridView1.Rows.Add(firstBitmap.Width);
+
+                for (int i = 0; i < firstBitmap.Width; i++)
+                {
+                    DataGridViewRow row2 = dataGridView1.Rows[i];
+                    dataGridView1.Columns[i].Width = 50;
+                }
+
+                await Task.Factory.StartNew(() =>
+                {
+                    for (int i = 0; i < firstBitmap.Width; i++)
+                    {
+                        for (int j = 0; j < firstBitmap.Width; j++)
+                        {
+                            dataGridView1[i, j].Value = (firstBitmap.GetPixel(i, j).R + firstBitmap.GetPixel(i, j).G + firstBitmap.GetPixel(i, j).B) / 3;
+                        }
+                    }
+
+                });
+
+
+                for (int i = 0; i < firstBitmap.Width; i++)
+                {
+                    for (int j = 0; j < firstBitmap.Width; j++)
+                    {
+                        var number = int.Parse(dataGridView1[j, i].Value.ToString()!);
+                        if (!listOfDifferentNumbers.Contains(number))
+                        {
+                            listOfDifferentNumbers.Add(number);
+                        }
+                    }
+                }
+
+
+                //add different numbers to list
+                for (int i = 0; i < firstBitmap.Width; i++)
+                {
+                    for (int j = 0; j < firstBitmap.Width; j++)
+                    {
+                        var number = int.Parse(dataGridView1[j, i].Value.ToString()!);
+                        if (!listOfDifferentNumbers.Contains(number))
+                        {
+                            listOfDifferentNumbers.Add(number);
+                        }
+                    }
+                }
+
+                for (int i = 0; i < firstBitmap.Width; i++)
+                {
+                    if (i == 0)
+                    {
+                        dataGridView2.Columns.Add(i + 1 + "", "");
+                    }
+                    dataGridView2.Columns.Add(i + 1 + "", i + 1 + "");
+
+                }
+
+                dataGridView2.Rows.Add(listOfDifferentNumbers.Count());
+
+                for (int i = 0; i < firstBitmap.Width; i++)
+                {
+                    dataGridView2.Columns[i].Width = 50;
+                }
+
+
+                //add to dataGridView2 different numbers to first column
+                for (int i = 0; i < listOfDifferentNumbers.Count(); i++)
+                {
+                    dataGridView2[0, i].Value = listOfDifferentNumbers[i];
+                }
+
+                var getPResult = await GetPByAngleAsync(90);
+
+                for (int i = 0; i < 15; i++)
+                {
+                    for (int j = 1; j < 50; j++)
+                    {
+                        dataGridView2[j, i].Value = getPResult[i, j - 1];
+                    }
+                }
+
+
+            }
+        }
+
+        private async Task<int[,]> GetPByAngleAsync(int angle)
+        {
+            var listOfDifferentNumbersCount = listOfDifferentNumbers.Count();
+
+            int[,] arrForP = new int[firstBitmap.Width, firstBitmap.Height];
+
+            for (int i = 0; i < firstBitmap.Width; i++)
+            {
+                for (int j = 0; j < firstBitmap.Height; j++)
+                {
+                    arrForP[i, j] = int.Parse(dataGridView1[i, j].Value.ToString());
+
+                    if (int.Parse(dataGridView1[j, i].Value.ToString()) == 53)
+                    {
+                        var ddd = 9;
+                    }
+                }
+            }
+
+            int[,] newArr = new int[50, 50];
+
+            var ii = 0;
+            var jj = 0;
+            var count = 0;
+
+            await Task.Factory.StartNew(() =>
+            {
+                foreach (var item in listOfDifferentNumbers)
+                {
+                    for (int step = 1; step < firstBitmap.Width; step++)
+                    {
+                        for (int row = 0; row < firstBitmap.Height; row++)
+                        {
+                            for (int col = 0; col < firstBitmap.Width; col++)
+                            {
+                                if (angle == 90 && col + step < 50 && arrForP[row, col] == item && arrForP[row, col + step] == item)
+                                {
+                                    count++;
+                                }
+                            }
+                        }
+
+                        newArr[ii, jj] = count;
+
+                        jj++;
+                        if (jj == 49)
+                        {
+                            jj = 0;
+                        }
+                        count = 0;
+                    }
+
+                    ii++;
+                }
+
+            });
+
+
+            return newArr;
+        }
+
+        private void pictureBox1_MouseHover(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Width = 300;
+            pictureBox1.Height = 300;
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBox1.Width = 50;
+            pictureBox1.Height = 50;
+            pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
